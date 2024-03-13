70c236478fc726752ee75ed91b2a2e10790482c1
xxingyiyuan <1391065515@qq.com>
2024-03-13 21:10:26 +0800

    feat: 移除无用代码 & button组件第一部分编码

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    :class="{
+      [`xx-button--${type}`]: type,
+      [`xx-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled
+    }"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+<script setup lang="ts">
+import type { ButtonProps } from './types'
+defineProps<ButtonProps>()
+defineOptions({
+  name: 'XxButton'
+})
+</script>
