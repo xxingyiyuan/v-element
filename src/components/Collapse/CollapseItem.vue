b40d73fe24873a7c7db2fbd41aa150915fb784c9
xxingyiyuan <1391065515@qq.com>
2024-03-17 17:41:49 +0800

    feat: collapse组件编码第二部分

--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="xx-collapse-item" :class="{ 'is-disabled': disabled }">
-    <div class="xx-collapse-item__header" :id="`item-header-${name}`">
+    <div class="xx-collapse-item__header" :id="`item-header-${name}`" @click="handleClick">
       <slot name="title">{{ title }}</slot>
     </div>
-    <div class="xx-collapse-item__content" :id="`item-content-${name}`">
+    <div class="xx-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
       <slot />
     </div>
   </div>
 </template>
 <script setup lang="ts">
+import { computed, inject } from 'vue'
 import type { CollapseItemProps } from './types'
+import { collapseContextKey } from './types'
 defineOptions({
   name: 'XxCollapseItem'
 })
-defineProps<CollapseItemProps>()
+const props = defineProps<CollapseItemProps>()
+const collapseContext = inject(collapseContextKey)
+const handleClick = () => {
+  if (props.disabled) {
+    return
+  }
+  collapseContext?.handleItemClick(props.name)
+}
+const isActive = computed(() => collapseContext?.activeNames.value.includes(props.name))
 </script>
